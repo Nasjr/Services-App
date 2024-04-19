614073b3f2b88eee635b88ae96fa55b9234a7b87
Mahmoud Nasser <100250722+Nasjr@users.noreply.github.com>
2024-04-19 07:57:29 +0200

    added the name of the application and the local stroage

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:ecommerce_application/features/authentication/Onboarding/views/onboarding.dart';
+import 'package:ecommerce_application/utils/constants/local_storage.dart';
 import 'package:ecommerce_application/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
   // Todo: add Widgits Binding
+  WidgetsFlutterBinding.ensureInitialized();
   // Todo: Init Local Storage
+  await Hive.initFlutter();
+  await Hive.openBox(LocalDataSourceBoxs.configBox);
   // Todo: Await Native Splash
   // Todo: Initialize Firebase
   // Todo: Initialize Authentication
-
-  runApp(const App());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const App()));
 }
 
 class App extends StatelessWidget {
